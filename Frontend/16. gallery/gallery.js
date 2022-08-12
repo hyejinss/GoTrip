0fe1075c9940b8e8ec6179cab95f82db972612f7
shhhaej <hyejin11sa@gmail.com>
2022-08-13 02:49:46 +0900

    Create a gallery page review slider.

--- Frontend/16. gallery/gallery.js
+++ Frontend/16. gallery/gallery.js
@@ -13,3 +13,36 @@ var functionName = "";
         }
         )}
   })
+
+
+
+// owl slide API
+$('.owl-carousel').owlCarousel({
+  items : 4, //한번에 보여줄 아이템 수
+  loop : true, //반복여부
+  margin : 35,
+  nav : true,
+  responsive : {
+      0 : {
+          items : 1
+      },
+      600 : {
+          items : 3
+      },
+      1000 : {
+          items : 4
+      }
+  }
+})
+
+// Go to the next item
+$('.customNextBtn').click(function() {
+  owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger('prev.owl.carousel', [300]);
+})
+
