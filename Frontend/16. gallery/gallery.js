76e9bfebf8932344fd6c280b144f65b9b1c200fe
shhhaej <hyejin11sa@gmail.com>
2022-08-17 01:04:09 +0900

    Gallery Page Travel Review Table Configuration.

--- Frontend/16. gallery/gallery.js
+++ Frontend/16. gallery/gallery.js
@@ -1,22 +1,4 @@
-var functionName = "";
-  $(function() {
-      var oldpageindex = sessionStorage.eventplanpageindex;
-      function bindpaging(){
-        //페이징 바인딩 [Get 방식]
-        $("#board__common").paging({
-            item: "a",
-            itemClass: "page number",
-            itemcurrent: "active",
-            length: 10,
-            current: 1,
-            max: 5149,
-        }
-        )}
-  })
-
-
-
-// owl slide API
+// Owl Carousel slide
 $('.owl-carousel').owlCarousel({
   items : 3, //한번에 보여줄 아이템 수
   loop : true, //반복여부
@@ -38,3 +20,34 @@ $('.owl-carousel').owlCarousel({
       }
   }
 });
+
+
+//Events by selected option
+function categoryChange(e) {
+  var region_overseas = ['유럽', '미주', '아프리카', '아시아'];
+  var region_domestic = ['서울', '부산', '여수', '제주'];
+  var target = document.getElementById('region'); 
+
+  if (e.value == 'overseas') var d = region_overseas;
+  else if (e.value == 'domestic') var d = region_domestic;
+
+  target.options.length = 0;
+
+  for (x in d) {
+    var opt = document.createElement('option');
+    opt.value = d[x];
+    opt.innerHTML = d[x];
+    target.appendChild(opt);
+  }
+}
+
+// $(document).ready(function() {
+//   $('#category').change(function() {
+//     var result = $('#category option:selected').val();
+//     if (result == 'overseas') {
+//       $('#region').show();
+//     } else {
+//       $('#region').hide();
+//     }
+//   });
+// });
